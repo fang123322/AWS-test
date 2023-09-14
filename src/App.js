dff0ffab80ff751abb42009969a2cf02b170f7d0
fanghongyang <459610318@qq.com>
2023-09-15 04:37:15 +0800

    test 1

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useRef } from 'react';
+import { Amplify, Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import Amplify from 'aws-amplify';
+Amplify.configure(awsconfig);
+
+import Axios from 'axios';
+import { nanoid } from 'nanoid'
+import './App.css';
+
+function App() {
+  const textData = useRef(null);
+  const fileData = useRef(null);
+  const id = nanoid();
+  function handleSubmit(event){
+    event.preventDefault()
+    const uploadUrl = 'https://wuzjwuf3y5.execute-api.ap-east-1.amazonaws.com/test1';
+    const dataUrl = 'https://wuzjwuf3y5.execute-api.ap-east-1.amazonaws.com/test1';
+    const file = fileData.files[0];
+    var fileBlob = URL.createObjectURL(file);
+    Storage.put(file.name, fileBlob, {
+      contentType: 'image/jpeg' // contentType is optional
+    });
+    Axios.post(uploadUrl, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(response => {
+      console.log('success', response.data);
+
+    }).catch(error => {
+      console.error('fill', error);
+    });
+  }
+  return (
+    <div className="flex flex-col h-screen bg-green-50 items-center justify-center view-account">
+    <div className="md:text-center ">
+      <div className="pt-6 md:p-8 space-y-4">
+        <form  className="font-medium" onSubmit={handleSubmit} action="https://wuzjwuf3y5.execute-api.ap-east-1.amazonaws.com/test1" method="post" encType="multipart/form-data" >
+          <div>
+              <label className="w-full md:w-1/2 ">Text input:</label>
+              <input className="w-full border md:w-1/2 " name="text" ref={textData}></input>
+              <br/>
+              <label className="w-full md:w-1/2 ">File input:</label>
+              <input className="w-full border md:w-1/2 " type="file" name="file" ref={fileData}></input>
+              <br/>
+              <button className="border m-4" type="submit">Submit</button>
+          </div>
+        </form >
+      </div>
+    </div>
+	</div>
+  );
+}
+
+export default App;
