3f40ec7ef6afd3a65bc362bec6679b4363a21366
fanghongyang <459610318@qq.com>
2023-09-15 05:57:39 +0800

    test4

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   const fileData = useRef(null);
   const id = nanoid();
   function handleSubmit(event){
-    debugger
     event.preventDefault()
     const uploadUrl = 'https://wuzjwuf3y5.execute-api.ap-east-1.amazonaws.com/test1';
     const dataUrl = 'https://wuzjwuf3y5.execute-api.ap-east-1.amazonaws.com/test1';
